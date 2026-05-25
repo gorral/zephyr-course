0adbaded2ed46ff254727675c209988232057892
Andrzej Najwer <a.najwer@telic.de>
2026-05-25 20:25:11 +0200

    Lecture 6: add LED sensor driver

---
+++ app/drivers/sensor/led_sensor/led_sensor.c
@@ -0,0 +1,104 @@
+#include <errno.h>
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
+
+LOG_MODULE_REGISTER(led_sensor, LOG_LEVEL_INF);
+
+#define DT_DRV_COMPAT gorral_led_sensor
+
+struct led_sensor_config {
+struct gpio_dt_spec led;
+};
+
+struct led_sensor_data {
+bool led_is_on;
+};
+
+static int led_sensor_sample_fetch(const struct device *dev,
+   enum sensor_channel chan)
+{
+const struct led_sensor_config *config = dev->config;
+struct led_sensor_data *data = dev->data;
+int ret;
+
+ARG_UNUSED(chan);
+
+ret = gpio_pin_set_dt(&config->led, 1);
+if (ret < 0) {
+return ret;
+}
+
+data->led_is_on = true;
+LOG_INF("sensor_sample_fetch: LED ON");
+
+return 0;
+}
+
+static int led_sensor_channel_get(const struct device *dev,
+  enum sensor_channel chan,
+  struct sensor_value *val)
+{
+const struct led_sensor_config *config = dev->config;
+struct led_sensor_data *data = dev->data;
+int ret;
+
+if (chan != SENSOR_CHAN_ALL) {
+return -ENOTSUP;
+}
+
+ret = gpio_pin_set_dt(&config->led, 0);
+if (ret < 0) {
+return ret;
+}
+
+data->led_is_on = false;
+
+if (val != NULL) {
+val->val1 = 0;
+val->val2 = 0;
+}
+
+LOG_INF("sensor_channel_get: LED OFF");
+
+return 0;
+}
+
+static int led_sensor_init(const struct device *dev)
+{
+const struct led_sensor_config *config = dev->config;
+
+if (!gpio_is_ready_dt(&config->led)) {
+LOG_ERR("LED GPIO device is not ready");
+return -ENODEV;
+}
+
+return gpio_pin_configure_dt(&config->led, GPIO_OUTPUT_INACTIVE);
+}
+
+static const struct sensor_driver_api led_sensor_api = {
+.sample_fetch = led_sensor_sample_fetch,
+.channel_get = led_sensor_channel_get,
+};
+
+#define LED_SENSOR_DEFINE(inst)\
+static struct led_sensor_data led_sensor_data_##inst;\
+\
+static const struct led_sensor_config led_sensor_config_##inst = {\
+.led = GPIO_DT_SPEC_INST_GET(inst, led_gpios),\
+};\
+\
+DEVICE_DT_INST_DEFINE(inst,\
+      led_sensor_init,\
+      NULL,\
+      &led_sensor_data_##inst,\
+      &led_sensor_config_##inst,\
+      POST_KERNEL,\
+      CONFIG_SENSOR_INIT_PRIORITY,\
+      &led_sensor_api);
+
+DT_INST_FOREACH_STATUS_OKAY(LED_SENSOR_DEFINE)
--- app/src/main.c
+++ app/src/main.c
@@ -1,46 +1,44 @@
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-#define APP_LED_NODE DT_ALIAS(app_led)
+#define LED_SENSOR_NODE DT_NODELABEL(led_sensor0)
 
-#if !DT_NODE_HAS_STATUS(APP_LED_NODE, okay)
-#error "Alias app-led is not defined in devicetree"
+#if !DT_NODE_HAS_STATUS(LED_SENSOR_NODE, okay)
+#error "led_sensor0 node is not defined in devicetree"
 #endif
 
-static const struct gpio_dt_spec app_led = GPIO_DT_SPEC_GET(APP_LED_NODE, gpios);
+static const struct device *const led_sensor = DEVICE_DT_GET(LED_SENSOR_NODE);
 
 int main(void)
 {
-	bool led_state = false;
+struct sensor_value value;
 
-	if (!gpio_is_ready_dt(&app_led)) {
-		LOG_ERR("app-led GPIO device is not ready");
-		return 0;
-	}
+if (!device_is_ready(led_sensor)) {
+LOG_ERR("LED sensor device is not ready");
+return 0;
+}
 
-	if (gpio_pin_configure_dt(&app_led, GPIO_OUTPUT_INACTIVE) < 0) {
-		LOG_ERR("Failed to configure app-led");
-		return 0;
-	}
+LOG_INF("LED sensor application started");
 
-	LOG_INF("Heartbeat started, period=%d ms", CONFIG_APP_HEARTBEAT_PERIOD_MS);
+while (1) {
+if (sensor_sample_fetch(led_sensor) < 0) {
+LOG_ERR("sensor_sample_fetch failed");
+return 0;
+}
 
-	while (1) {
-		if (gpio_pin_toggle_dt(&app_led) < 0) {
-			LOG_ERR("Failed to toggle app-led");
-			return 0;
-		}
+k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
 
-		led_state = !led_state;
-		LOG_INF("Heartbeat LED: %s, period=%d ms",
-			led_state ? "ON" : "OFF",
-			CONFIG_APP_HEARTBEAT_PERIOD_MS);
+if (sensor_channel_get(led_sensor, SENSOR_CHAN_ALL, &value) < 0) {
+LOG_ERR("sensor_channel_get failed");
+return 0;
+}
 
-		k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
-	}
+k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+}
 
-	return 0;
+return 0;
 }
