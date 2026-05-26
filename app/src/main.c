591af34f777eefda00f45253353eb9b2a23e9192
Andrzej Najwer <a.najwer@telic.de>
2026-05-27 01:02:02 +0200

    Lecture 7: expose LED sensor custom API in shell

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,20 @@
+#include <errno.h>
+#include <stdlib.h>
+
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/shell/shell.h>
 
+#include "led_sensor.h"
+
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 #define LED_SENSOR_NODE DT_NODELABEL(led_sensor0)
+
+#define SENSOR_SET_MIN_VALUE 0
+#define SENSOR_SET_MAX_VALUE 100
 
 #if !DT_NODE_HAS_STATUS(LED_SENSOR_NODE, okay)
 #error "led_sensor0 node is not defined in devicetree"
@@ -73,10 +81,56 @@ shell_print(sh, "ready state: %s", device_is_ready(led_sensor) ? "ready" : "not 
 return 0;
 }
 
+static int cmd_sensor_set(const struct shell *sh, size_t argc, char **argv)
+{
+char *endptr;
+long value;
+int ret;
+
+if (argc < 2) {
+shell_error(sh, "missing value");
+shell_error(sh, "usage: sensor set <%d-%d>",
+    SENSOR_SET_MIN_VALUE, SENSOR_SET_MAX_VALUE);
+return -EINVAL;
+}
+
+errno = 0;
+value = strtol(argv[1], &endptr, 10);
+
+if ((errno != 0) || (*endptr != '\0')) {
+shell_error(sh, "invalid value: %s", argv[1]);
+return -EINVAL;
+}
+
+if ((value < SENSOR_SET_MIN_VALUE) || (value > SENSOR_SET_MAX_VALUE)) {
+shell_error(sh, "value out of range: %ld", value);
+shell_error(sh, "allowed range: %d-%d",
+    SENSOR_SET_MIN_VALUE, SENSOR_SET_MAX_VALUE);
+return -ERANGE;
+}
+
+if (!device_is_ready(led_sensor)) {
+shell_error(sh, "LED sensor device is not ready");
+return -ENODEV;
+}
+
+ret = led_sensor_set_runtime_parameter(led_sensor, (int32_t)value);
+if (ret < 0) {
+shell_error(sh, "led_sensor_set_runtime_parameter failed: %d", ret);
+return ret;
+}
+
+shell_print(sh, "runtime parameter set to %ld", value);
+
+return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(sensor_subcommands,
 SHELL_CMD(fetch, NULL, "Call sensor_sample_fetch() and turn LED on.", cmd_sensor_fetch),
 SHELL_CMD(read, NULL, "Call sensor_channel_get() and turn LED off.", cmd_sensor_read),
 SHELL_CMD(info, NULL, "Print sensor device name and ready state.", cmd_sensor_info),
+SHELL_CMD_ARG(set, NULL, "Set runtime parameter. Usage: sensor set <0-100>",
+      cmd_sensor_set, 2, 0),
 SHELL_SUBCMD_SET_END
 );
 
@@ -90,7 +144,7 @@ return 0;
 }
 
 LOG_INF("LED sensor shell application started");
-LOG_INF("Try shell commands: sensor info, sensor fetch, sensor read");
+LOG_INF("Try shell commands: sensor info, sensor fetch, sensor read, sensor set <value>");
 
 return 0;
 }
